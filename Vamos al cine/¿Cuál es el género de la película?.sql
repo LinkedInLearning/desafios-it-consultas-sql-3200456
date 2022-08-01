88c0502138dc4fc67e4450e0b86fe601c893175b
Lincy <lincgr@gmail.com>
2022-07-31 21:36:56 -0600

    ¿Cuál es el género de la película?

---
+++ Vamos al cine/¿Cuál es el género de la película?.sql
@@ -0,0 +1,15 @@
+CREATE TABLE genero (
+  id INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(100) NULL,
+  PRIMARY KEY (id)
+);
+  
+/* INSERT */
+INSERT INTO `genero`(`nombre`)VALUES('Acción');
+INSERT INTO `genero`(`nombre`)VALUES('Aventura');
+INSERT INTO `genero`(`nombre`)VALUES('Ciencia Ficción');
+INSERT INTO `genero`(`nombre`)VALUES('Comedia');
+INSERT INTO `genero`(`nombre`)VALUES('Drama');
+INSERT INTO `genero`(`nombre`)VALUES('Fantasía');
+INSERT INTO `genero`(`nombre`)VALUES('Suspenso');
+INSERT INTO `genero`(`nombre`)VALUES('Terror');
